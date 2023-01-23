a3d109d98ec19d26e236c05f8fe2c25d2c396686
Jessica Fealy <jessicaf091@outlook.com>
2023-01-23 10:14:45 +0000

    Changed to TS and added openapi

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Configuration, BlinkyBackEndApi } from "./generated-sources/openapi";
+
+const configuration = new Configuration({
+  basePath: "http://localhost:5127",
+});
+
+function App() {
+  useEffect(() => {
+    const api = new BlinkyBackEndApi(configuration);
+    api.testGet().then((response) => {
+      console.log(response);
+    });
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
