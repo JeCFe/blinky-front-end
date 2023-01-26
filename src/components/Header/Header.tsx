9ddd20083bfa5bf54721d0d55530ee5aef0bf1e3
Michal <michal@ms.pl>
2023-01-26 11:48:02 +0000

    added simposon fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import "./Header.css";
 export const Header = () => {
   return (
     <header className="App-header">
-      <p className="App-logo">BLINKY </p>
+      <p className="App-logo">BLINKY</p>
     </header>
   );
 };
